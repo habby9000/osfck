0547d95953fd60e1470ed1f93f31e82cb7ef4087
habby9000 <gamego511@naver.com>
2017-11-06 16:12:26 +0900

    asd

--- js/demo/demo.js
+++ js/demo/demo.js
@@ -53,39 +53,63 @@ $(function () {
   // Initialize the Gallery as video carousel:
   blueimp.Gallery([
     {
-      title: 'Sintel',
+      title: '1',
       href: 'https://archive.org/download/Sintel/' +
         'sintel-2048-surround.mp4',
       type: 'video/mp4',
-      poster: 'https://i.imgur.com/MUSw4Zu.jpg'
+      poster: 'http://image.chosun.com/sitedata/image/201704/18/2017041801159_0.jpg'
     },
     {
-      title: 'Big Buck Bunny',
+      title: '2',
       href: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/' +
         'Big_Buck_Bunny_4K.webm',
       type: 'video/webm',
-      poster: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/' +
-        'Big_Buck_Bunny_4K.webm/4000px--Big_Buck_Bunny_4K.webm.jpg'
+      poster: 'http://image.chosun.com/sitedata/image/201704/18/2017041801159_1.jpg'
     },
     {
-      title: 'Elephants Dream',
+      title: '3',
       href: 'https://upload.wikimedia.org/wikipedia/commons/8/83/' +
         'Elephants_Dream_%28high_quality%29.ogv',
       type: 'video/ogg',
-      poster: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/' +
-        'Elephants_Dream_s1_proog.jpg/800px-Elephants_Dream_s1_proog.jpg'
+      poster: 'http://image.chosun.com/sitedata/image/201704/18/2017041801159_3.jpg'
     },
     {
-      title: 'LES TWINS - An Industry Ahead',
-      type: 'text/html',
-      youtube: 'zi4CIXpx7Bg'
-    },
-    {
-      title: 'KN1GHT - Last Moon',
+      title: '4',
       type: 'text/html',
       vimeo: '73686146',
-      poster: 'https://secure-a.vimeocdn.com/ts/448/835/448835699_960.jpg'
+      poster: 'http://image.chosun.com/sitedata/image/201704/18/2017041801159_4.jpg'
+    },
+    {
+      title: '5',
+      type: 'text/html',
+      vimeo: '73686146',
+      poster: 'http://image.chosun.com/sitedata/image/201704/18/2017041801159_5.jpg'
+    },
+    {
+      title: '6',
+      type: 'text/html',
+      vimeo: '73686146',
+      poster: 'http://image.chosun.com/sitedata/image/201704/18/2017041801159_6.jpg'
+    },
+    {
+      title: '7',
+      type: 'text/html',
+      vimeo: '73686146',
+      poster: 'http://image.chosun.com/sitedata/image/201704/18/2017041801159_7.jpg'
+    },
+    {
+      title: '8',
+      type: 'text/html',
+      vimeo: '73686146',
+      poster: 'http://image.chosun.com/sitedata/image/201704/18/2017041801159_8.jpg'
+    },
+    {
+      title: '9',
+      type: 'text/html',
+      vimeo: '73686146',
+      poster: 'http://image.chosun.com/sitedata/image/201704/18/2017041801159_9.jpg'
     }
+    
   ], {
     container: '#blueimp-video-carousel',
     carousel: true
